23539a75c41cf624ab72b1073be44c7e16dbacc5
Levi-21cm <99470417+Levi-21cm@users.noreply.github.com>
2022-02-25 11:04:12 +0100

    prím vagy nem prím?

---
+++ 34feladat/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner konzol = new Scanner(System.in);
+        System.out.println("adja meg a szám");
+        int szam = konzol.nextInt();
+
+        if (szam %2==0){
+            System.out.println("nem prím");
+        }
+        else if (szam<2){
+            System.out.println("nem prím");
+        }
+        else {
+            System.out.println("prím");
+        }
+    }
+}
