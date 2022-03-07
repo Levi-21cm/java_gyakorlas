95efddcb5ee584f4643f165a2e119dafb53b6b8d
Levi-21cm <99470417+Levi-21cm@users.noreply.github.com>
2022-03-07 10:05:13 +0100

    ascii valami

---
+++ 41felada/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	for (int i =97;i<122;i++){
+        System.out.printf("%c %d\n", (char)i,i);
+    }
+    }
+}
